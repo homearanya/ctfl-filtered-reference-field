4ad3af51fab062824cf4fda7bb760c73031c846c
Carlos Gonzalez <homearanya@gmail.com>
2021-10-20 09:06:30 +0200

    Add console.log

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -130,7 +130,7 @@ const Dialog = (props: DialogProps) => {
       }
     }
   }, [entryFilter, setEntryFilter, entries])
-
+  console.log({ selectedRelatedField })
   return (
     <Flex padding="spacingXl" flexDirection="column">
       <Flex marginBottom="spacingL">
